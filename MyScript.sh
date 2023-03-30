1a3ea97490553d8b966d78855b49dff601a60980
SAch <SAch@gmail.com>
2023-03-30 20:40:40 +0530

    ADDed Shell Script

---
+++ MyScript.sh
@@ -0,0 +1,10 @@
+HEAD is a reference to the last commit in the currently checked-out branch.
+• origin refers to the source repository from where it was cloned. 
+• git diff : View difference between Stage and Working Directory
+• git diff --staged : View difference between HEAD and Staged
+• git diff HEAD : View difference between HEAD and Working Directory
+- Staged and index both are same
+- Unstaged changes exist in our Working Directory, but Git hasn’t recorded 
+them into its version history yet. 
+- Staged changes are a lot like unstaged changes, except that they’ve been 
+marked to be committed the next time you run git commit
